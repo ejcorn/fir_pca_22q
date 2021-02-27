a0b649f8752a20682ae76d91d82c7ae6991765fe
Eli Cornblath <eli.cornblath@uphs.upenn.edu>
2021-02-26 19:29:39 -0500

    add fin and st to job

--- jobs/fir_design.sh
+++ jobs/fir_design.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-$MP -nodisplay -r "name_root = char('$D'); basedir = char('$BD'); cd(basedir); addpath(genpath('code')); run([basedir,'code/fir/fir_design.m']); exit"
+$MP -nodisplay -r "name_root = char('$D'); basedir = char('$BD'); fin=$FIN; st = $ST; cd(basedir); addpath(genpath('code')); run([basedir,'code/fir/fir_design.m']); exit"
